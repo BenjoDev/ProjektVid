03619e1525ef2b8f203feec09ee49190599699d0
BenjoDev <benjamintrzan@protonmail.com>
2023-05-31 14:42:30 +0200

    improved face recognition

--- newFaceRecognition.py
+++ newFaceRecognition.py
@@ -1,25 +1,23 @@
 import cv2
 import numpy as np
-from keras.models import load_model  # TensorFlow is required for Keras to work
+from keras.models import load_model  
 
 def face_recognition(pot_slike):
     name = "other"
-    facedetect = cv2.CascadeClassifier('har.xml')
-    model = load_model('keras_model.h5')
+    np.set_printoptions(suppress=True)
 
-    imgOrignal = cv2.imread(pot_slike)
-    faces = facedetect.detectMultiScale(imgOrignal,1.3,5)
+    model = load_model("keras_Model.h5", compile=False)
 
-    for x,y,w,h in faces:
-        crop_img=imgOrignal[y:y+h,x:x+h]
-        img=cv2.resize(crop_img, (224,224))
-        img=img.reshape(1, 224, 224, 3)
-        prediction=model.predict(img)
-        classIndex = np.argmax(prediction,axis=-1)
-        print(classIndex[0])
-        if(classIndex[0] == 1):
-            name = "benjamin"
-        elif(classIndex[0] == 2):
-            name = "zan"
-        return name # 0 = other, 1 = Benjamin, 2 = Zan
-    return name
+    image = cv2.imread(pot_slike)
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+    image = (image / 127.5) - 1
+
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+
+    if(index == 1):
+        name = "benjamin"
+    elif(index == 2):
+        name = "zan"
+    return name # 0 = other, 1 = Benjamin, 2 = Zan
